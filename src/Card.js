b6d4bff17ff814730f5e88e60718e6461737d00f
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-13 13:58:04 +0300

    implement airbnb card components

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Card.css"
+
+// We need props which is a functional component.
+// When we add props, we need to destructure it to use.
+// Since we will use a lot of card component, we use props like src and we minimize time consuming as well as the number of folders in a file.
+ 
+function Card({src, title, description, price}) {
+    return (
+        <div className="card">
+             <img src={src} />
+             <div className="card__info">
+                <h2>{title}</h2>
+                <h4>{description}</h4>
+                <h3>{price}</h3>
+             </div>
+        </div>
+    )
+}
+
+export default Card
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Home.css";
 import Banner from "./Banner";
+import Card from "./Card";
 
 // ES7 snippets to do "rfce"
 
@@ -8,6 +9,29 @@ function Home() {
     return (
         <div className="home">
             <Banner />
+
+            <div className="home__section">
+                <Card 
+                    src="https://a0.muscache.com/im/pictures/18cf5f05-0318-430a-8c0d-7e8b4a056c7c.jpg?im_w=1440"
+                    title="Online Experience"
+                    description="Unique activities we can do together, led by a world of hosts."
+                />
+                <Card 
+                    src="https://a0.muscache.com/im/pictures/18cf5f05-0318-430a-8c0d-7e8b4a056c7c.jpg?im_w=1440"
+                    title="Online Experience"
+                    description="Unique activities we can do together, led by a world of hosts."
+                />
+                <Card 
+                    src="https://a0.muscache.com/im/pictures/18cf5f05-0318-430a-8c0d-7e8b4a056c7c.jpg?im_w=1440"
+                    title="Online Experience"
+                    description="Unique activities we can do together, led by a world of hosts."
+                />
+            </div>
+            <div className="home__section">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
