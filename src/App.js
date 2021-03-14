3fb72b1eb63004deb874e50a77d2ab533c537c57
berkayyildirim <berkay.yildirim@yahoo.com>
2021-03-14 17:57:10 +0300

    implement airbnb footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
     <Header/>
       <Home/>
+    <Footer />
 
     {/* Home */}
        {/* Header */}
